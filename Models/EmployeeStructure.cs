48a66ac482304a770d877cfdcc7838ec15cc5924
glock <glockajulia@gmail.com>
2025-08-10 20:08:09 +0200

    fix db docker-compose

--- Models/EmployeeStructure.cs
+++ Models/EmployeeStructure.cs
@@ -2,6 +2,8 @@
 {
     public class EmployeeStructure
     {
-
+        public int EmployeesId { get; set; }
+        public int SuperiorId { get; set; }
+        public int SuperiorLeveled { get; set; }
     }
 }
