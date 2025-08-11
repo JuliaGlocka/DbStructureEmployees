011b08d9b34efab0223b7480f3a5baf915c2f38b
glock <glockajulia@gmail.com>
2025-08-11 14:09:40 +0200

    modify Employee.cs, add Team, Vacation, VacationPackage clasess, fix EmployeeStructureTests.cs

---
+++ Models/Vacation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DbStructureEmployees.Models
+{
+    public class Vacation
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DateSince { get; set; }
+        public DateTime DateUntil { get; set; }
+        public double NumberOfHours { get; set; }
+        public bool IsPartialVacation { get; set; }
+        public int EmployeeId { get; set; }
+        public virtual Employee Employee { get; set; } = null!;
+    }
+}
